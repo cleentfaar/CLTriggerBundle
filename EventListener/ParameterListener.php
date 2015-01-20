40fa7c7fda1059f15fb9d07ad2e9271004101b6c
Cas Leentfaar <git@casleentfaar.com>
2015-01-20 23:18:12 +0100

    fixed tests

--- EventListener/ParameterListener.php
+++ EventListener/ParameterListener.php
@@ -4,6 +4,8 @@ namespace CL\Bundle\TriggerBundle\EventListener;
 
 use CL\Bundle\TriggerBundle\Util\ParameterHandlerRegistry;
 use CL\Bundle\TriggerBundle\Util\RedirectHelper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class ParameterListener
@@ -37,33 +39,34 @@ class ParameterListener
             return;
         }
 
-        $this->tryParameterHandlers($event);
-
-        if ($event->getResponse()) {
-            // fasten the response
-            $event->stopPropagation();
-        }
-    }
-
-    /**
-     * @param GetResponseEvent $event
-     */
-    private function tryParameterHandlers(GetResponseEvent $event)
-    {
-        $request = $event->getRequest();
         foreach ($request->query->all() as $key => $value) {
             foreach ($this->parameterHandlerRegistry->getHandlers($key) as $handlerData) {
-                list($handler, $method) = $handlerData;
-
-                $redirectHelper = new RedirectHelper($request, [$key]);
-                $response       = $handler->{$method}($value, $redirectHelper);
+                $response = $this->executeHandler($request, $handlerData, $key, $value);
 
                 if ($response !== null) {
                     $event->setResponse($response);
+                    $event->stopPropagation();
 
                     return;
                 }
             }
         }
+    }
+
+    /**
+     * @param Request $request
+     * @param array   $handlerData
+     * @param string  $parameter
+     * @param string  $value
+     *
+     * @return Response|null
+     */
+    private function executeHandler(Request $request, array $handlerData, $parameter, $value)
+    {
+        list($handler, $method) = $handlerData;
+
+        $redirectHelper = new RedirectHelper($request, [$parameter]);
+
+        return $handler->{$method}($value, $redirectHelper);
     }
 }
