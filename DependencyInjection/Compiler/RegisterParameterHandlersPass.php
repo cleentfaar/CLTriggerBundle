e76ea8e73c791aaa23aa5a77999bb2267ddacb15
Cas Leentfaar <git@casleentfaar.com>
2015-01-20 22:58:08 +0100

    fixed compiler pass

--- DependencyInjection/Compiler/RegisterParameterHandlersPass.php
+++ DependencyInjection/Compiler/RegisterParameterHandlersPass.php
@@ -11,14 +11,27 @@ class RegisterParameterHandlersPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $definition = $container->getDefinition('cl_trigger.util.parameter_handler_registry');
+        $tag        = 'cl_trigger.parameter_handler';
 
-        foreach ($container->findTaggedServiceIds('cl_trigger.parameter_handler') as $id => $tags) {
+        foreach ($container->findTaggedServiceIds($tag) as $id => $tags) {
             foreach ($tags as $tagAttributes) {
-                if (isset($tagAttributes['parameter'])) {
-                    $definition->addMethodCall('registerParameterHandler', [new Reference($id), $tagAttributes['parameter']]);
-                } else {
-                    $definition->addMethodCall('registerParameterBagHandler', [new Reference($id)]);
+                if (!isset($tagAttributes['parameter'])) {
+                    throw new \InvalidArgumentException(sprintf(
+                        'Services marked with the tag %s must define a value for the `%s` attribute',
+                        $tag,
+                        'parameter'
+                    ));
                 }
+
+                if (!isset($tagAttributes['method'])) {
+                    throw new \InvalidArgumentException(sprintf(
+                        'Services marked with the tag %s must define a value for the `%s` attribute',
+                        $tag,
+                        'method'
+                    ));
+                }
+
+                $definition->addMethodCall('register', [new Reference($id), $tagAttributes['method'], $tagAttributes['parameter']]);
             }
         }
     }
